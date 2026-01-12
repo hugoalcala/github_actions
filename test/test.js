9ee973a6617f24b38d3caf98722c64c122bbd88c
hugoalcala <125819614+hugoalcala@users.noreply.github.com>
2026-01-12 10:35:13 +0100

    Rename test suite and update animal validation

--- test/test.js
+++ test/test.js
@@ -1,16 +1,16 @@
 const app = require('../app.js');
 const request = require('supertest')(app);
 
-describe('Pruebas de la Granja', function(){
-    it('La API debe responder con éxito y tener los nuevos animales', function(done){
-        request
-        .get('/api')
-        .set('Accept', 'application/json')
-        .expect(200)
-        .expect(function(res) {
-            if (!res.body.duck) throw new Error("Falta el pato en app.js");
-            if (!res.body.horse) throw new Error("Falta el caballo en app.js");
-        })
-        .end(done);
-    });
+describe('Validación de la Granja', function(){
+  it('La API debe incluir los nuevos animales', function(done){
+    request
+      .get('/api')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .expect(function(res) {
+        if (!res.body.duck) throw new Error("No encuentro al pato");
+        if (!res.body.horse) throw new Error("No encuentro al caballo");
+      })
+      .end(done);
+  });
 });
