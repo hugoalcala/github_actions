bb2c44688755ec4a08fd345f65b4fd19bd5c792b
hugoalcala <125819614+hugoalcala@users.noreply.github.com>
2026-01-12 10:33:24 +0100

    Refactor tests for API response and animals

--- test/test.js
+++ test/test.js
@@ -1,39 +1,16 @@
 const app = require('../app.js');
 const request = require('supertest')(app);
 
-describe('GET', function(){
-	  it('respuesta contiene text/html', function(done){
-		      request
-		      .get('/')
-		      .set('Accept', 'text/html')
-		      .expect('Content-Type', /html/)
-		      .expect(200, done);
-		    })
-
-	  it('respuesta contiene George Orwell', function(done){
-		      request
-		      .get('/')
-		      .set('Accept', 'text/html')
-		      .expect(200, /George Orwell had a farm/ig, done);
-		    })
-
-	  it('/api respuesta contiene json', function(done){
-		      request
-		      .get('/api')
-		      .set('Accept', 'application/json')
-		      .expect('Content-Type', /json/)
-		      .expect(200, done);
-		    })
-
-	it('/api respuesta contiene los animales nuevos', function(done){
-		      request
-		      .get('/api')
-		      .set('Accept', 'application/json')
-		      .expect(200)
-              .expect(function(res) {
-                if (!res.body.duck) throw new Error("Falta el pato");
-                if (!res.body.horse) throw new Error("Falta el caballo");
-              })
-              .end(done);
-		    });
-})
+describe('Pruebas de la Granja', function(){
+    it('La API debe responder con éxito y tener los nuevos animales', function(done){
+        request
+        .get('/api')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .expect(function(res) {
+            if (!res.body.duck) throw new Error("Falta el pato en app.js");
+            if (!res.body.horse) throw new Error("Falta el caballo en app.js");
+        })
+        .end(done);
+    });
+});
